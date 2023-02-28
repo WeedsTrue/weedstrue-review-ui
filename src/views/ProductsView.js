e4484f8281247ea35ff844000d372f9deded0ee5
Graham <Grahamwormond@gmail.com>
2023-02-27 22:19:12 -0500

    WR-2-Products | Display list and detailed view (#2)

--- src/views/ProductsView.js
+++ src/views/ProductsView.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import ProductDetails from '../components/content/products/ProductDetails';
+import ProductListView from '../components/content/products/ProductListView';
 
 const ProductsView = () => {
   return (
     <Routes>
-      <Route element={<></>} path="/" />
+      <Route element={<ProductListView />} path="/" />
+      <Route element={<ProductDetails />} path="/:uuid" />
       <Route element={<Navigate replace to="/products" />} path="*" />
     </Routes>
   );
